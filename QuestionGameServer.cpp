ba529d146d6a20d32bcb0604c22c736bd9d05663
jbreslin33 <jbreslin33@localhost>
2010-11-16 12:13:32 -0500

    ...

--- QuestionGameServer.cpp
+++ QuestionGameServer.cpp
@@ -5,10 +5,12 @@ Filename:    QuestionGameServer.cpp
 
 #include "QuestionGameServer.h"
 
+#include <iostream>
+
 //-------------------------------------------------------------------------------------
 QuestionGameServer::QuestionGameServer()
 {
-	
+	std::cout << "Creating QuestionGameServer\n";
 }
 //-------------------------------------------------------------------------------------
 QuestionGameServer::~QuestionGameServer(void)
